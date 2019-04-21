1bc4e60897c4eb43efd1349722baf60f98ff7b98
PeterKahenya <Kahenya0@gmail.com>
2019-04-21 23:43:21 +0300

    Added accounts app, created the articles list and articles detail functionaliy

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Article
+from django.http import HttpResponse
 
 def article_list(request):
-    return render(request,"articles/article_list.html")
+    articles=Article.objects.all().order_by('date')
+    return render(request,"articles/article_list.html",{'articles':articles})
+
+def article_detail(request,slug):
+    #return HttpResponse("detail with slug <strong>"+slug+"<strong>")
+    article=Article.objects.get(slug=slug)
+    return render(request,"articles/article_detail.html",{'article':article})
